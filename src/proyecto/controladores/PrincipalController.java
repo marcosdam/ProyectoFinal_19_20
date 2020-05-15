48dc0a97d4627b435811d783d86e3a8387e59161
Marcos <marcos.ledesma.picazo@cieep.com>
2020-05-15 18:07:37 +0200

    Cambios en la BD y en las clases

--- src/proyecto/controladores/PrincipalController.java
+++ src/proyecto/controladores/PrincipalController.java
@@ -1,4 +1,17 @@
 package proyecto.controladores;
 
-public class PrincipalController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrincipalController implements Initializable {
+
+    private BaseDatos controlDB;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Crear BD
+        controlDB = new BaseDatos();
+    }
 }
